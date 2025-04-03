10d4dad867ae9d529aa88f1b734c1ce0581dbd37
hdemarcay <hdemarcay@ufl.edu>
2025-04-03 08:56:45 -0400

    trying to add new matching not working

--- Assets/Scripts/MatchingGameScripts/DraggableBone.cs
+++ Assets/Scripts/MatchingGameScripts/DraggableBone.cs
@@ -1,27 +1,27 @@
 // SamiyyahRucker(MadeLove)
 // Makes bones moveable and matchable
 
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class DraggableBone : MonoBehaviour
 {
     public string boneId; // match this bone to its correct target
 
-    private Vector3 correctPosition; // where the bone should snap to
-    private Quaternion correctRotation; // roatation when snapped
     private bool isPlaced = false; // locks bone if correctly matched
     private bool isBeingDragged = false; // tracks if bone is currently being dragged
+
+    private Transform reference; 
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private float initialScreenX;
 
     void Start()
     {
         // ask manager for the correct position for reference bone
-        Transform reference = MatchingGameManager.Instance.GetReferenceTransform(boneId);
-        if (reference != null)
-        {
-            correctPosition = reference.position;
-            correctRotation = reference.rotation;
-        }
-        else
+        reference = MatchingGameManager.Instance.GetReferenceTransform(boneId);
+        if (reference == null)
         {
             Debug.LogWarning($"Reference not found for bone: {boneId}");
         }
@@ -32,14 +32,21 @@ public class DraggableBone : MonoBehaviour
         
         if (isBeingDragged && Input.GetMouseButtonUp(0)) // drag and realease 
         {
+            GameObject obj=reference.gameObject;
+            Renderer renderer = obj.GetComponentInChildren<Renderer>();
+            renderer.material.SetColor("_Color", new Color(0, 0, 1, 0.1f));
+
             isBeingDragged = false; // stops drag state
 
-            float dist = Vector3.Distance(transform.position, correctPosition);
+            float dist = Vector3.Distance(transform.position, reference.position);
+            Debug.Log(dist);
+
             if (dist < 0.5f)
             {
-                transform.position = correctPosition;
-                transform.rotation = correctRotation;
+                transform.position = reference.position;
+                transform.rotation = reference.rotation;
                 isPlaced = true; // snaps in place locks and cant move again
+                reference.gameObject.SetActive(false);
 
                 if (GetComponent<Rigidbody>() != null)
                     Destroy(GetComponent<Rigidbody>());
@@ -52,9 +59,17 @@ public class DraggableBone : MonoBehaviour
 
     void OnMouseDown() // activates drag when pressed and bone isn’t placed
     {
+        GameObject obj=reference.gameObject;
+        Renderer renderer = obj.GetComponentInChildren<Renderer>();
+        renderer.material.SetColor("_Color", new Color(1, 0, 0, 0.2f));
+
         if (!isPlaced)
         {
+            initialPosition=transform.position;
+            initialRotation=transform.rotation;
+            initialScreenX=Camera.main.WorldToScreenPoint(transform.position).x;
             isBeingDragged = true;
+            
         }
     }
 
@@ -62,6 +77,20 @@ public class DraggableBone : MonoBehaviour
     {
         if (!isPlaced)
         {
+            
+            Vector3 refp=Camera.main.WorldToScreenPoint(reference.position);
+            refp.y=0;refp.z=0;
+            Vector3 p=Camera.main.WorldToScreenPoint(transform.position);
+            p.y=0;p.z=0;
+            float dist=Vector3.Distance(refp,new Vector3(initialScreenX,0,0));
+            Vector3 delta=p-refp;
+
+            float w=delta.x/dist;
+            Debug.Log(this.name+" "+dist);
+            if(w<0)w=0;else if(w>1)w=1;
+            transform.position=Vector3.Lerp(reference.position,initialPosition,w);
+            transform.rotation=Quaternion.Lerp(reference.rotation,initialRotation,w);
+
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = Camera.main.WorldToScreenPoint(transform.position).z;
             transform.position = Camera.main.ScreenToWorldPoint(mousePos);
